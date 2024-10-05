81e121376c4a054cdd5a746264a4f90beb76605b
KiwiBoat <110491827+KiwiBoat@users.noreply.github.com>
2024-10-05 07:46:59 +0200

    feat: authentication setup

--- back-end/index.js
+++ back-end/index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import authenticationRoutes from "./routes/authentication.js";
+
 // Load environment variables
 dotenv.config();
 
 const application = express(); // Create an express application
 const PORT = process.env.PORT || 1000; // Default port is 1000
 
+application.use("/api/authentication", authenticationRoutes); 
+
+// Define a route for the root of the application
 application.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
